4e7d9b261e392be8e5a6b816dc0ab326e0bbb264
DanielRojasF <danielrf98@gmail.com>
2023-02-08 13:02:28 -0600

    Creacion del Proyecto inicial

---
+++ src/test/java/com/TiendaM/TiendaMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
