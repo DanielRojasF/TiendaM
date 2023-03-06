705e9530d5160c85348e640c7aea7a35b1e5576f
DanielRojasF <danielrf98@gmail.com>
2023-03-05 20:45:34 -0600

    Commit #5.0

--- src/main/java/com/TiendaM/controller/IndexController.java
+++ src/main/java/com/TiendaM/controller/IndexController.java
@@ -1,39 +1,52 @@
 package com.TiendaM.controller;
 
-import com.TiendaM.dao.ClienteDao;
 import com.TiendaM.domain.Cliente;
-import java.util.Arrays;
-import java.util.List;
-import javax.management.AttributeValueExp;
+import com.TiendaM.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
 public class IndexController {
     
     @Autowired
-    ClienteDao clienteDao;
+    ClienteService clienteService;
     
     @GetMapping("/")
     public String inicio(Model model) {
-//        log.info("Ahora desde MVC");
-//        model.addAttribute("Mensaje","Hola desde el controlador");
-//        
-//        Cliente cliente = new Cliente("Daniel", "Rojas Fuentes", "danielrf98@gmail.com", "86741454");
-//        model.addAttribute("objetoCliente", cliente);
-//        
-//        Cliente cliente2 = new Cliente("Juan", "Rojas Fuentes", "danielrf98@gmail.com", "86741454");
-//        Cliente cliente3 = new Cliente("Pedro", "Rojas Fuentes", "danielrf98@gmail.com", "86741454");
-//        
-//        List<Cliente> clientes = Arrays.asList(cliente, cliente2, cliente3);
+        log.info("Ahora desde MVC");
 
-        var clientes = clienteDao.findAll();
+        var clientes = clienteService.getClientes();
         model.addAttribute("clientes", clientes);
                 
         return "index";
+    }
+    
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "modificarCliente";
+    }
+    
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model){
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+    
+    @GetMapping("eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente, Model model){
+        clienteService.delete(cliente);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/TiendaM/service/ClienteService.java
@@ -0,0 +1,15 @@
+package com.TiendaM.service;
+
+import com.TiendaM.domain.Cliente;
+import java.util.List;
+
+
+public interface ClienteService {
+    public List<Cliente> getClientes();/*firma del metodo*/
+    
+    public Cliente getCliente(Cliente cliente);
+    
+    public void save (Cliente cliente);//Para insertar o modificar (Si viene el idCliente o no)
+
+    public void delete (Cliente cliente);
+}
